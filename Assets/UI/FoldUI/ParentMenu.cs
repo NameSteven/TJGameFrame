9d4985945b942b8556c41eae5456e5c25d390c45
NameSteven <876130540@qq.com>
2019-02-28 14:47:25 +0800

    增加折叠式的滑动列表

---
+++ Assets/UI/FoldUI/ParentMenu.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ParentMenu : MonoBehaviour
+{
+    private GameObject childMenu;//子菜单的parent
+    private RectTransform[] childs;//所有子菜单的rect
+    private RectTransform itemRect;//子菜单的prefab
+    private Vector3 offset;//单个子菜单的高度
+    private int count;//子菜单的个数
+    public bool isOpening { get; private set; }//父菜单是否展开
+    public bool isCanClick { get; set; }//父菜单是否可以点击
+
+    public void Init(RectTransform rect, int count,Type t, IList dataList=null)
+    {
+        if (!typeof(ParentItem).IsAssignableFrom(t)) throw new Exception("传入类型必须是ParentItem的派生类");
+        childMenu = transform.Find("childMenu").gameObject;
+        itemRect = rect;
+        this.count = count;
+        childs = new RectTransform[this.count];
+        offset = new Vector3(0, itemRect.rect.height);
+        for (int i = 0; i < this.count; i++)
+        {
+            childs[i] = Instantiate(itemRect, childMenu.transform);
+
+            ParentItem parentComponent = childs[i].GetComponent(t)as ParentItem;
+            if (parentComponent == null) {
+                parentComponent = childs[i].gameObject.AddComponent(t) as ParentItem;
+            }
+            parentComponent.FindItem(childs[i]);
+            parentComponent.FillItem(dataList[i]);
+            childs[i].gameObject.SetActive(true);
+        }
+        childMenu.gameObject.SetActive(false);
+        isOpening = false;
+        isCanClick = true;
+        GetComponent<Button>().onClick.AddListener(OnButtonClick);
+    }
+
+    void OnButtonClick()
+    {
+        if (!isCanClick) return;
+        if (!isOpening)
+            StartCoroutine(ShowChildMenu());
+        else
+            StartCoroutine(HideChildMenu());
+    }
+
+    IEnumerator ShowChildMenu()
+    {
+        childMenu.gameObject.SetActive(true);
+        for (int i = 0; i < count; i++)
+        {
+            childs[i].localPosition -= i * offset;
+            yield return new WaitForSeconds(0.01f);
+        }
+        isCanClick = true;
+        isOpening = true;
+    }
+
+    IEnumerator HideChildMenu()
+    {
+        for (int i = count - 1; i >= 0; i--)
+        {
+            childs[i].localPosition += i * offset;
+            yield return new WaitForSeconds(0.01f);
+        }
+        childMenu.gameObject.SetActive(false);
+        isCanClick = true;
+        isOpening = false;
+    }
+}
---
+++ Assets/UI/FoldUI/ScrollTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScrollTest : MonoBehaviour {
+
+  public   FoldableMenu foldadleMenu;
+
+    Dictionary<int, List<string>> DataDic;
+    // Use this for initialization
+    void Start () {
+        DataDic = new Dictionary<int, List<string>>();
+
+        DataDic[1] = new List<string>() { "12","222","33","23232",};
+        DataDic[2] = new List<string>() { "12", "222", "sds33", "23232", };
+        DataDic[3] = new List<string>() { "1sds2", "2sds22", "33", "23232", };
+        DataDic[4] = new List<string>() { "1sds2", "22sd2", "33", "23232", };
+        DataDic[5] = new List<string>() { "12", "2sds22", "3sdds3", "23232", };
+
+        foldadleMenu.CreatScrool(DataDic, typeof(Parent), typeof(Child));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+public class Parent : ParentItem {
+
+    private Text text;
+    public override void FindItem(Transform trans) {
+        base.FindItem(trans);
+        text = trans.Find("Text").GetComponent<Text>();
+    }
+
+    public override void FillItem(object parm) {
+        base.FillItem(parm);
+        text.text = ((int)parm).ToString();
+    }
+}
+
+public class Child : ParentItem {
+    private Text text;
+    public override void FindItem(Transform trans) {
+        base.FindItem(trans);
+        text = trans.Find("Text").GetComponent<Text>();
+    }
+
+    public override void FillItem(object parm) {
+        base.FillItem(parm);
+        text.text = parm.ToString();
+    }
+}
